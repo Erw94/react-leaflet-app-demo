d4f0108f717f0fb9fb3ab0a8ae43dd0418d0f761
Erw94 <einar.wiegaard@gmail.com>
2022-04-04 17:21:58 +0200

    ok1

--- src/App.js
+++ src/App.js
@@ -1,54 +1,29 @@
-import './App.css';
 import React, {useState} from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { Icon } from "leaflet";
-import * as parkData from "./data/skateboard-parks.json";
+import useSwr from "swr";
+import './App.css';
 
-function App() {
-  const [activePark, setActivePark] = useState(null);
-  const icon = new Icon({
-    iconUrl: "/skateboarding.svg",
-    iconSize: [25, 25]
-  });
+const fetcher = (...args) => fetch(...args).then(response => response.json());
+
+export default function App() {
+  const url =
+    "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
+  const { data, error } = useSwr(url, { fetcher });
+  const crimes = data && !error ? data.slice(0, 100) : [];
 
   return (
-    <MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false}>
-  <TileLayer
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  />
-  {parkData.features.map(park => (
+    <MapContainer center={[52.6376, -1.135171]} zoom={12} scrollWheelZoom={false}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+
+      {crimes.map(crime => (
         <Marker
-          key={park.properties.PARK_ID}
-          position={[
-            park.geometry.coordinates[1],
-            park.geometry.coordinates[0]
-          ]}
-          onClick={() => {
-            setActivePark(park);
-          }}
-          icon={icon}
+          key={crime.id}
+          position={[crime.location.latitude, crime.location.longitude]}
         />
       ))}
-
-      {activePark && (
-        <Popup
-          position={[
-            activePark.geometry.coordinates[1],
-            activePark.geometry.coordinates[0]
-          ]}
-          onClose={() => {
-            setActivePark(null);
-          }}
-        >
-          <div>
-            <h2>{activePark.properties.NAME}</h2>
-            <p>{activePark.properties.DESCRIPTIO}</p>
-          </div>
-        </Popup>
-      )}
-</MapContainer>
+    </MapContainer>
   );
 }
-
-export default App;
